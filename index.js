5f718f31a0bc45e86e616be58ad0bc8001045cc6
Vipul Chauhan <viratdev07@gmail.com>
2021-01-03 06:48:48 +0530

    Take notes

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // used to safeguard that the script has loaded before executing the index.js
 $(document).ready(function () {
-    $('h1').css('color', 'lightgreen');
+    $('h1').css('color', 'green');
 })
+
+
+// Some important methods 
+.hasAttribute
+.attr
+.text
+.html
+.classList
+
+
+
+
