4950fb1042e4a4dc79abed690214e53485615e9b
Vipul Chauhan <viratdev07@gmail.com>
2021-01-03 07:24:41 +0530

    added event listener in jQuery

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-// used to safeguard that the script has loaded before executing the index.js
-$(document).ready(function () {
-    $('h1').css('color', 'green');
-})
+$("h1").addClass("big");
 
-
-// Some important methods 
-.hasAttribute
-.attr
-.text
-.html
-.classList
-
-
-
-
+$(document).keypress(function(e) {
+    console.log($('h1').text(e.key));
+});
